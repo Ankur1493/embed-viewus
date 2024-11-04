1c8c7fc000dff9f1f1fc668493e4f8f45f0cc773
Udit Kapoor <uditkapoor060@gmail.com>
2024-11-04 19:33:36 +0530

    Created testimonialGridHorizontal

--- src/pages/HallOfFame.tsx
+++ src/pages/HallOfFame.tsx
@@ -1,16 +1,21 @@
 import TestimonialGrid1 from "../components/TestimonialGrid1";
 import TestimonialGrid2 from "../components/TestimonialGrid2";
+import TestimonialGridHorizontal from "@/components/TestimonialGridHorizontal";
 
 function HallOfFame() {
   return (
     <div className="py-10">
-      <div className="mx-10">
+      <div className="mx-20">
         <p className="text-xl font-bold underline">TestimonialGrid1</p>
         <TestimonialGrid1 />
       </div>
       <div className="mx-20 py-10">
         <p className="text-xl font-bold underline">TestimonialGrid2</p>
         <TestimonialGrid2 />
+      </div>
+      <div className="mx-20 py-10">
+        <p className="text-xl font-bold underline">TestimonialGridHorizontal</p>
+        <TestimonialGridHorizontal />
       </div>
     </div>
   );
